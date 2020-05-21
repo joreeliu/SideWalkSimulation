2b4239c4b4f9975db3c3c63aca73c9de13b15859
yy2792 <yy2792@columbia.edu>
2020-05-20 23:23:47 -0400

    update person png

--- Shared/GameConstants.py
+++ Shared/GameConstants.py
@@ -3,12 +3,12 @@ import os
 
 class GameConstants:
     SCREEN_SIZE = [1000, 800]
-    PERSON_SIZE = [50, 50]
+    PERSON_SIZE = [5, 5]
     BLOCK_SIZE = [50, 50]
 
     SPRITE_PERSON = os.path.join("Assets", "Person.png")
     SPRITE_BLOCK = os.path.join("Assets", "iron.png")
 
     SOCIAL_DISTANCE = 2
-    PERSON_SPEED = 50
+    PERSON_SPEED = 2
     TICK = 5000
